9b791e67fa742448554ff4e66fed1e5b3c329b78
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-05-05 19:37:28 +0000

    cohort info added + switch to hashes

--- directory.rb
+++ directory.rb
@@ -1,33 +1,34 @@
 # let's crate an array and put all our sutents inside it
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"]
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
 # let's create a few methods we will use for printing what we need
 # first one is going to be used to print the header
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
 end
-# second method is to print the names in the array
-def print_names(names)
-  names.each do |name|
-    puts name
+# second method has now changed in order to interact with multipe info regarding the students
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end 
 end
-# third method is used to print the footer
-def print_footer(names)
-puts "Overall, we have #{names.count} great students "
+# third method is used to print the footer 
+def print_footer(students)
+puts "Overall, we have #{students.count} great students "
 end
 # nothing will happen till we call our methods with the correct parameters
 print_header
-print_names(students)
+print(students)
 print_footer(students)
