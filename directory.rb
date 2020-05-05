27108512ea4fecc3bb30b01f22ed2d0518a33ec9
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-05-05 13:41:35 +0000

    defined brand-new methods to execute our printing needs

--- directory.rb
+++ directory.rb
@@ -11,12 +11,23 @@ students = [
   "The Joker",
   "Joffrey Baratheon",
   "Norman Bates"]
-# now let's print all the students iterating over the array elements
-puts "The students of Villains Academy"
-puts "------------"
-students.each do |name|
-  puts name
-end 
-# we print the total using a count method (it returns the number of elements of an array)
-puts "Overall, we have #{students.count} great students "
-
+# let's create a few methods we will use for printing what we need
+# first one is going to be used to print the header
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+# second method is to print the names in the array
+def print_names(names)
+  names.each do |name|
+    puts name
+  end 
+end
+# third method is used to print the footer
+def print_footer(names)
+puts "Overall, we have #{names.count} great students "
+end
+# nothing will happen till we call our methods with the correct parameters
+print_header
+print_names(students)
+print_footer(students)
