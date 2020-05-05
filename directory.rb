144153960136906727afa3b1080dd8e0aaa7d1a5
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-05-05 20:36:30 +0000

    Created input_students method + interactive program

--- directory.rb
+++ directory.rb
@@ -1,34 +1,44 @@
-# let's crate an array and put all our sutents inside it
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# let's create a few methods we will use for printing what we need
-# first one is going to be used to print the header
+# Instead of manually feeding our array of students we are now going to ask the user to do that
+# First thing to do is to create a method
+def input_students
+  puts "Please, enter the name of the students"
+  puts "To finish, just hit the return twice"
+  # we create an empty array that will contain all the input names
+  students = []
+  # we get the first name
+  name = gets.chomp
+  # let's create a loop that, while the name is not an empty string, will execute an action
+  while !name.empty? do
+    # the first action is to add the students to our students array
+    students << {name: name, cohort: :november}
+    # the second one is to update the user with the current number of students
+    puts "Now we have #{students.count} students"
+    # now we ask for the next name
+    name = gets.chomp
+  end 
+  # after the loop break, our program's last action is to return the the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
 end
-# second method has now changed in order to interact with multipe info regarding the students
+# second method has now changed in order to interact with multipe info regarding the 
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end 
 end
-# third method is used to print the footer 
+# third method is used to print the footer
 def print_footer(students)
 puts "Overall, we have #{students.count} great students "
 end
 # nothing will happen till we call our methods with the correct parameters
+
+# now, instead of having a precompiled array of students, we'll use our methods
+# with a new students variable that is given by our new method input_students
+students = input_students
 print_header
 print(students)
 print_footer(students)
